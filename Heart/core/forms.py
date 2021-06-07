6128bfbd1f8524e0b41ec5e1cc5b1f94e9bbc199
ROMIT-DAS <romitdas2@gmail.com>
2021-06-07 21:51:51 +0530

    This is ML part

--- Heart/core/forms.py
+++ Heart/core/forms.py
@@ -1,5 +1,25 @@
 from django import forms
-
-class CreateFormForHeart(forms.From):
-    name=forms.CharField(max_length=100)
-    
+select_sex = (("0","Female",), ("1","Male"))
+select_cp = (("0","typical angina"),("1","atypical angina"),("2","non-anginal pain"),("3","asymptomatic"))
+select_fbs = (("1","Yes"),("2","No"))
+select_restecg = (("0","Normal"),("1","Having ST-T wave abnormality"),("2","Showing probable or definite left"))
+select_exang = (("1","Yes"),("0","No"))
+select_slope = (("1","Upsloping"),("2","Flat"),("3","Downsloping"))
+select_ca=(("0","0"),("1","1"),("2","2"),("3","3"))
+select_thal = (("0","Normal"),("1","Fixed defect"),("2","Blood flow normal"),("3","Reversible defect"))
+class CreateFormForHeart(forms.Form):
+    name=forms.CharField(max_length=100 )
+    sex=forms.ChoiceField(choices=select_sex)
+    cp=forms.ChoiceField(choices=select_cp)
+    age=forms.IntegerField()
+    trestbps=forms.IntegerField()
+    chol=forms.IntegerField()
+    fbs=forms.ChoiceField(choices=select_fbs)
+    restecg=forms.ChoiceField(choices=select_restecg)
+    thalach = forms.IntegerField()
+    exang = forms.ChoiceField(choices=select_exang)
+    oldpeak=forms.FloatField()
+    slope=forms.ChoiceField(choices=select_slope)
+    ca=forms.ChoiceField(choices=select_ca)
+    thal=forms.ChoiceField(choices=select_thal)
+# max_length trestbps
--- Heart/core/views.py
+++ Heart/core/views.py
@@ -1,9 +1,63 @@
 from django.shortcuts import render,HttpResponse
+from .forms import CreateFormForHeart
+import sklearn
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score,mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+
+heart=pd.read_csv('D:\Machine Learning\Project\heart.csv')
+x=heart
+y=heart.target
+x.drop('target',axis=1,inplace=True)
+sc = StandardScaler()
+X = sc.fit_transform(x)
+x_train,x_test,y_train,y_test=train_test_split(X,y,random_state=10,test_size=0.3,shuffle=True)
+
+# this is for KNN
+dt=DecisionTreeClassifier()
+dt.fit(x_train,y_train)
+
+
+
 
 # Create your views here.
 def form_view(request):
-    return render(request ,template_name='Mainapp/index.html')
+    if request.method=="POST":
+        print(request.POST)
+        fm=CreateFormForHeart(request.POST)
+        
+        if fm.is_valid():
+            user=fm.cleaned_data['name']
+            age=fm.cleaned_data['age']
+            sex=fm.cleaned_data['sex']
+            cp=fm.cleaned_data['cp']
+            trestbps=fm.cleaned_data['trestbps']
+            chol=fm.cleaned_data['chol']
+            fbs=fm.cleaned_data['fbs']
+            restecg=fm.cleaned_data['restecg']
+            thalach=fm.cleaned_data['thalach']
+            exang=fm.cleaned_data['exang']
+            oldpeak=fm.cleaned_data['oldpeak']
+            slope=fm.cleaned_data['ca']
+            thal=fm.cleaned_data['thal']
+            ca=fm.cleaned_data['ca']
+    #             index = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach',
+    #    'exang', 'oldpeak', 'slope', 'ca', 'thal']
+
+    #             for list in index:
+    #                  print( list=fm.cleaned_data[index[list]])
+
+            X_DT=np.array([[age ,sex, cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,thal,ca]])
+            X_DT=sc.transform(X_DT)
+            X_DT_prediction=dt.predict(X_DT)
+            print(X_DT_prediction[0])
+    else:
+        fm=CreateFormForHeart()
+    return render(request ,'Mainapp/index.html',{'form':fm})
 
 
 def resault_view(request):
-    return render(request ,template_name='Mainapp/resault.html')
+    return render(request ,'Mainapp/resault.html')
